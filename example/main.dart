ecf09ae7f295064f46ec6af8adf023437f84bd20
Carl Hain <crhain@gmail.com>
2019-05-27 14:34:57 -0500

    Add test for printOptions method to main.dart

--- example/main.dart
+++ example/main.dart
@@ -1,9 +1,15 @@
 import 'package:prompter_crh/src/terminal.dart';
+import 'package:prompter_crh/src/option.dart';
 
 void main() {
   Terminal terminal = Terminal();
-  terminal.clearScreen();
-  terminal.printPrompt('Hello World!');
-  var input = terminal.collectInput();
-  print('You just entered: $input');
+  var options = [
+    Option('I want red', '#f00'),
+    Option('I want blue', '#00f'),
+  ];
+
+  terminal.printOptions(options);
+  var response = terminal.collectInput();
+
+  print(response);
 }
