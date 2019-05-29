7869b68d1ac724231a959969d5eaacd7798fdf21
Carl Hain <crhain@gmail.com>
2019-05-29 18:15:27 -0500

    Add Ask method to Prompter class

--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -1,1 +1,11 @@
-class Prompter {}
+import 'option.dart';
+import 'terminal.dart';
+
+class Prompter {
+  void ask(String prompt, List<Option> options) {
+    var terminal = new Terminal();
+
+    terminal.printPrompt(prompt);
+    terminal.printOptions(options);
+  }
+}
