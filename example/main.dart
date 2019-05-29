922d809b2e15b37beecaef01e4844075287e59eb
Carl Hain <crhain@gmail.com>
2019-05-29 18:18:14 -0500

    Update main.dart to test Prompter ask method

--- example/main.dart
+++ example/main.dart
@@ -1,15 +1,12 @@
-import 'package:prompter_crh/src/terminal.dart';
 import 'package:prompter_crh/src/option.dart';
+import 'package:prompter_crh/src/prompter.dart';
 
 void main() {
-  Terminal terminal = Terminal();
   var options = [
     Option('I want red', '#f00'),
     Option('I want blue', '#00f'),
   ];
 
-  terminal.printOptions(options);
-  var response = terminal.collectInput();
-
-  print(response);
+  var prompter = Prompter();
+  prompter.ask("What is your favorite color?", options);
 }
