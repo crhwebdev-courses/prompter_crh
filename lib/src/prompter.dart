ac40c424c8bfdf26778f8890f0aab1d71039b2fd
Carl Hain <crhain@gmail.com>
2019-06-02 12:41:22 -0500

    Update ask method on Prompter class to return the option value for item selected

--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -4,8 +4,12 @@ import 'terminal.dart';
 final Terminal _terminal = const Terminal();
 
 class Prompter {
-  void ask(String prompt, List<Option> options) {
+  ask(String prompt, List<Option> options) {
     _terminal.printPrompt(prompt);
     _terminal.printOptions(options);
+
+    final String input = _terminal.collectInput();
+
+    return options[int.parse(input)].value;
   }
 }
