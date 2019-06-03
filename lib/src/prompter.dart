6b8278f6c33ff6852a261711f947bfc9a8b98c73
Carl Hain <crhain@gmail.com>
2019-06-03 15:44:20 -0500

    Add askBinary method to Prompter. This asks a yes/no question and returns true if yes entered

--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -4,6 +4,14 @@ import 'terminal.dart';
 final Terminal _terminal = const Terminal();
 
 class Prompter {
+  bool askBinary(String prompt) {
+    _terminal.printPrompt('$prompt (y/n)');
+
+    final input = _terminal.collectInput();
+
+    return input.contains('y');
+  }
+
   askMultiple(String prompt, List<Option> options) {
     _terminal.printPrompt(prompt);
     _terminal.printOptions(options);
